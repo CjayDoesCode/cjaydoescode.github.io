e489ec26762ea0ff5f2d602ed45442dadd003fca
CjayDoesCode <cjaycodes@protonmail.com>
2025-06-07 08:35:24 +0800

    feat: add structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <main>
+      <header>
+        <h1>Cjay Muñoz, 20</h1>
+        <p>Software Developer</p>
+        <p>Angeles City, Philippines</p>
+        <nav>
+          <ul>
+            <li>
+              <a href="mailto:cjay.munoz@protonmail.com" title="Send an email">Email</a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/CjayDoesCode"
+                target="_blank"
+                rel="noreferrer"
+                title="View GitHub profile"
+              >
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://linkedin.com/in/cjay-muñoz/"
+                target="_blank"
+                rel="noreferrer"
+                title="View LinkedIn profile"
+              >
+                LinkedIn
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <section>
+        <h2>About me</h2>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          What's up! I'm Cjay Muñoz, a first year Computer Science student.
+          I love solving coding problems and tinkering with both computer software and hardware.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </section>
+      <section>
+        <h2>Tech Stack</h2>
+        <ul>
+          <li>C</li>
+          <li>C++</li>
+          <li>CSS</li>
+          <li>HTML</li>
+          <li>Java</li>
+          <li>JavaScript</li>
+          <li>Next.js</li>
+          <li>Python</li>
+          <li>React</li>
+          <li>SQL</li>
+          <li>Tailwind CSS</li>
+          <li>TypeScript</li>
+          <li>Vite</li>
+        </ul>
+      </section>
+      <footer>
+        <p>
+          This web app was built with React, Tailwind CSS, and Vite. You can view the source code on&nbsp;
+          <a
+            href="https://github.com/CjayDoesCode"
+            target="_blank"
+            rel="noreferrer"
+            title="GitHub repository"
+          >
+            GitHub
+          </a>.
+        </p>
+      </footer>
+    </main>
   )
 }
 
